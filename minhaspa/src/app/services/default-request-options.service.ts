b7f87ac002ef38f485109f8a3842f33d26d81e09
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2017-12-22 10:59:18 -0200

    Unificando opções da requisição

--- minhaspa/src/app/services/default-request-options.service.ts
+++ minhaspa/src/app/services/default-request-options.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {RequestOptions, RequestOptionsArgs, Headers} from "@angular/http";
+import {JwtTokenService} from "./jwt-token.service";
 
 @Injectable()
-export class DefaultRequestOptionsService {
+export class DefaultRequestOptionsService extends RequestOptions {
 
-  constructor() { }
+  constructor(private jwtToken:JwtTokenService) {
+    super();
+  }
 
+  //fazendo override- faz merge entre configurações já existente e as que eu passar
+  merge(options?: RequestOptionsArgs): RequestOptions {
+    let headers = options.headers || new Headers();
+    headers.set('Authorization', `Bearer ${this.jwtToken.token}`);
+    headers.set('Content-Type', 'application/json');
+    options.headers = headers;
+    return super.merge(options);
+  }
 }
