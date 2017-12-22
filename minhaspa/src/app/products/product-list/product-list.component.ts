78d733cc249850c2185c81f4cadbd5947aa4e83a
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2017-12-22 11:32:05 -0200

    Refresh Token

--- minhaspa/src/app/products/product-list/product-list.component.ts
+++ minhaspa/src/app/products/product-list/product-list.component.ts
@@ -4,6 +4,7 @@ import {Http, RequestOptions, Headers} from "@angular/http";
 import 'rxjs/add/operator/toPromise';
 import {AuthService} from "../../services/auth.service";
 import {DefaultRequestOptionsService} from "../../services/default-request-options.service";
+import {JwtTokenService} from "../../services/jwt-token.service";
 
 @Component({
   selector: 'app-product-list',
@@ -14,7 +15,7 @@ export class ProductListComponent implements OnInit {
   products:Array<Object> = [];
 
   //constructor(private http:Http, private jwtToken:JwtTokenService,
-  constructor(private http:Http, private auth: AuthService, private requestOptions:DefaultRequestOptionsService){
+  constructor(private http:Http, private auth: AuthService, private requestOptions:DefaultRequestOptionsService,private jwtToken:JwtTokenService){
     //console.log(this.auth.check);
   }
 
@@ -37,6 +38,19 @@ export class ProductListComponent implements OnInit {
     this.http
         .get('http://localhost:8000/api/products', this.requestOptions.merge(new RequestOptions()))
         .toPromise()
-        .then(response => this.products = response.json());
+        .then(response => this.products = response.json())
+        .catch(responseError=>{
+            if(responseError.status == '401'){
+                this.http.post('http://localhost:8000/api/refresh_token', {}, this.requestOptions.merge(new RequestOptions()))
+                    .toPromise()
+                    .then(response => {
+                        this.jwtToken.token = response.json().token;
+                        this.http
+                            .get('http://localhost:8000/api/products', this.requestOptions.merge(new RequestOptions()))
+                            .toPromise()
+                            .then(response=> this.products = response.json())
+                    })
+            }
+        });
   }
 }
